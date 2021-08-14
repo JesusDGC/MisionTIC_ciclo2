92fa443579b70b1fb2135fb17e6caa1cb1347ce9
jesusdgc123 <83377823+jesusdgc123@users.noreply.github.com>
2021-08-14 11:27:09 -0500

    versión 1.1 retos del ciclo 2 MisiónTIC2021

---
+++ Semana2/RetoJava/CompanyManager/src/main/java/classes/clsAdministrative.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author Jesus Garcia
+ */
+public class clsAdministrative {
+    private String category;
+    private String subordinates;
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    /**
+     * @return the subordinates
+     */
+    public String getSubordinates() {
+        return subordinates;
+    }
+
+    /**
+     * @param subordinates the subordinates to set
+     */
+    public void setSubordinates(String subordinates) {
+        this.subordinates = subordinates;
+    }
+}
---
+++ Semana2/RetoJava/CompanyManager/src/main/java/classes/clsEmployee.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author Jesus Garcia
+ */
+public class clsEmployee extends clsPerson{
+    private int salary;
+    private clsTrade trade;
+    private clsAdministrative administrative;
+
+    public clsEmployee(int salary, clsTrade trade, String name, String lastName, String identification, String e_mail) {
+        super(name, lastName, identification, e_mail);
+        this.salary = salary;
+        this.trade = trade;
+    }
+
+
+    /**
+     * @return the salary
+     */
+    public int getSalary() {
+        return salary;
+    }
+
+    /**
+     * @param salary the salary to set
+     */
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    /**
+     * @return the administrative
+     */
+    public clsAdministrative getAdministrative() {
+        return administrative;
+    }
+
+    /**
+     * @param administrative the administrative to set
+     */
+    public void setAdministrative(clsAdministrative administrative) {
+        this.administrative = administrative;
+    }
+
+    /**
+     * @return the trade
+     */
+    public clsTrade getTrade() {
+        return trade;
+    }
+
+    /**
+     * @param trade the trade to set
+     */
+    public void setTrade(clsTrade trade) {
+        this.trade = trade;
+    }
+    
+}
