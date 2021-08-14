c352bc3d0ca87c832be0b9d98a53bbdd4e8d1448
jesusdgc123 <83377823+jesusdgc123@users.noreply.github.com>
2021-08-14 09:58:19 -0500

    versión 1 del reto de la semana 2 del ciclo 2 MisionTIC

---
+++ RetoJava/CompanyManager/src/main/java/classes/clsPerson.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author Jesus Garcia
+ */
+public class clsPerson {
+    private String name;
+    private String lastName;
+    private String identification;
+    private String e_mail;
+
+    
+    public clsPerson(String name, String lastName, String identification, String e_mail) {
+        this.name = name;
+        this.lastName = lastName;
+        this.identification = identification;
+        this.e_mail = e_mail;
+    }
+    
+    
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the identification
+     */
+    public String getIdentification() {
+        return identification;
+    }
+
+    /**
+     * @param identification the identification to set
+     */
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+
+    /**
+     * @return the e_mail
+     */
+    public String getE_mail() {
+        return e_mail;
+    }
+
+    /**
+     * @param e_mail the e_mail to set
+     */
+    public void setE_mail(String e_mail) {
+        this.e_mail = e_mail;
+    }
+    
+}
